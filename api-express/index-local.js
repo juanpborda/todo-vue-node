b4ff46379419890a720a6510804e4f5717b4982e
Juan <juampiib@gmail.com>
2019-05-27 11:10:41 -0300

    more netlify

---
+++ api-express/index-local.js
@@ -0,0 +1,9 @@
+const app = require('./index');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const port = process.env.PORT || 5000;
+
+app.use('/api/todos', app.todos);
+app.listen(port, () => {
+    debug(`Listening on port ${chalk.green(port)}`);
+});
--- api-express/index.js
+++ api-express/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const chalk = require('chalk');
-const debug = require('debug')('app');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,14 +13,8 @@ app.use(bodyParser.json());
 
 const todos = require('./routes/api/todo');
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/.netlify/functions/server', todos);  // path must route to lambda
-} else {
-    app.use('/api/todos', todos);
-    app.listen(port, () => {
-        debug(`Listening on port ${chalk.green(port)}`);
-    });
-}
+app.use('/.netlify/functions/server', todos);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
+module.exports.todos = todos;
